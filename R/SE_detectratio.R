f451201c76818b670e9fcaec06297392a5982cfd
shaoxunyuan <ysx0226@gmail.com>
2025-03-18 16:24:51 +0800

    update

--- R/SE_detectratio.R
+++ R/SE_detectratio.R
@@ -26,72 +26,59 @@ SE_detectratio <- function(SE, assayname = "TPM", group_col = "group") {
         stop(paste("Assay", assayname, "not found in SE."))  
     }  
     
-    # Get feature information and expression matrix  
-    feature_info <- rowData(SE)  
-    expdata <- assay(SE, assayname)  
+	# Get feature/sample information and expression matrix  
+    feature_info <- as.data.frame(rowData(SE)  )
+    sample_info <- as.data.frame(colData(SE))
+    expdata <- as.data.frame(assay(SE, assayname))
     
     # Calculate detection samples and ratios  
     detect_samples <- rowSums(expdata != 0)  
     total_samples <- ncol(expdata)  
     
     # Update feature_info with detection sample counts and ratios  
-    feature_info[["detectsample"]] <- detect_samples  
-    feature_info[["detectratio"]] <- round(detect_samples / total_samples, 4)  
+    feature_info[,"detectsample"] <- detect_samples  
+    feature_info[,"detectratio"] <- round(detect_samples / total_samples, 4)  
 
     # Ensure the group column exists  
     if (!group_col %in% colnames(colData(SE))) {  
         stop("The colData must contain a column named 'group'.")  
     }    
-    group_info <- colData(SE)[[group_col]]  
     
-    # Split expression data by group  
-    split_matrices <- split(as.data.frame(expdata), group_info)  
-    
-    # Function to calculate detection samples and ratios per group  
+    group_info <- unique(colData(SE)[[group_col]]);group_info
+	
+	split_matrices = list()
+	for(selectgroup in group_info){
+		expdata_sub = expdata[,colnames(expdata) %in% rownames(sample_info[sample_info[,group_col] == selectgroup,])]
+		split_matrices[[selectgroup]] = expdata_sub
+	}
+	
+	# Function to calculate detection samples and ratios per group  
     Detect_sampleandratio <- function(data, selectgroup) {  
         detect_samples <- rowSums(data != 0)  
         total_samples <- ncol(data)  
         
-        feature_info[[paste0("detectsample_", selectgroup)]] <- detect_samples  
-        feature_info[[paste0("detectratio_", selectgroup)]] <- round(detect_samples / total_samples, 4)  
+        feature_info[,paste0("detectsample_", selectgroup)] <- detect_samples  
+        feature_info[,paste0("detectratio_", selectgroup)] <- round(detect_samples / total_samples, 4)  
         
         return(feature_info)  
     }  
 
     # Calculate detection ratios for each group  
-    for(selectgroup in names(split_matrices)) {  
+    for(selectgroup in group_info) {  
         feature_info <- Detect_sampleandratio(split_matrices[[selectgroup]], selectgroup)  
     }  
 
     # Update SE's rowData with the updated feature_info  
-    rowData(SE) <- feature_info  
-
-    # Create a plot of detection ratios  
-    non_zero_count <- rowSums(!is.na(expdata))  
-    plot_data <- data.frame(Feature = names(non_zero_count), Count = non_zero_count)  
-    plot_data <- plot_data[order(-plot_data$Count), ]  
-    plot_data$Feature <- factor(plot_data$Feature, levels = plot_data$Feature)  
-    plot_data$Fraction <- round(plot_data$Count / ncol(expdata), 2)  
-
-    # Plot sample counts  
-    plot1 <- ggplot(plot_data, aes(x = Feature, y = Count)) +   
-        geom_col(fill = "steelblue") +   
-        labs(x = paste0("Feature: ", nrow(expdata)), y = paste0("Sample Count: ", ncol(expdata)), title = "") +      
-        theme_minimal() +   
-        theme(axis.text.x = element_blank(), axis.ticks.x = element_blank(), panel.grid = element_blank(),   
-              plot.background = element_rect(fill = "white", color = NA))  
-
+    rowData(SE) <- feature_info   
+	
     # Plot histogram of detection ratios  
-    plot2 <- ggplot(as.data.frame(rowData(SE)), aes(x = detectratio)) +  
+    plot <- ggplot(as.data.frame(rowData(SE)), aes(x = detectratio)) +  
         geom_histogram(binwidth = 0.05, fill = "steelblue", color = "black", alpha = 0.7) +  
         geom_density(aes(y = ..count.. * 0.05), color = "salmon", size = 1) +  
         labs(title = "",  x = "Detection Ratio",  y = "Count") +  
         scale_x_continuous(breaks = seq(0, 1, by = 0.1)) +  
         theme_minimal() +  
         theme(axis.text.x = element_blank(), axis.ticks.x = element_blank(), panel.grid = element_blank())  
-
-    # Combine plots  
-    plot <- plot_grid(plot1, plot2, ncol = 2, align = "h")  
 
     return(list(SE = SE, plot = plot))  
 }
