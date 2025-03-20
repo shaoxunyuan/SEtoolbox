24815b0ce0373c48e2af2089e315960cff3cb4ce
shaoxunyuan <ysx0226@gmail.com>
2025-03-20 10:31:33 +0800

    update

--- R/SE_boxplot.R
+++ R/SE_boxplot.R
@@ -6,7 +6,7 @@
 #' @param group_col Column name in colData(SE) for grouping.
 #' @param normalization Normalization method ("scale", "log", or "none").
 #' @export
-SE_boxplot <- function(SE, feature_of_interest = c("AAK1", "ABCA13", "ABCB10", "ABCC1"), assayname = "TPM", group_col = NA, normalization = "none") {  
+SE_boxplot <- function(SE, feature_of_interest = c("AAGAB", "ABCA13", "ABCC4", "ABHD2"), assayname = "TPM", group_col = NA, normalization = "none") {  
     library(SummarizedExperiment)  
     library(reshape2)  
     library(ggplot2)  
@@ -33,28 +33,44 @@ SE_boxplot <- function(SE, feature_of_interest = c("AAK1", "ABCA13", "ABCB10", "
         exp_data_subset <- log2(exp_data_subset)  # 对数据进行 log1p 转换  
     }  # 如果 sel_mode 是 "none"，则保留原始数据  
 
-    # 如果提供了分组且不是 NA，则获取分组信息  
-    if (!is.na(group_col) && group_col %in% names(colData(SE))) {  
+    # 如果提供了分组且不是 NULL，则获取分组信息  
+    if (!is.null(group_col) && group_col %in% names(colData(SE))) {  
         group_info <- colData(SE)[[group_col]]  
         exp_data_melted <- melt(as.matrix(exp_data_subset))  
         exp_data_melted$group <- rep(group_info, each = nrow(exp_data_subset))  
         
         # 绘制 boxplot（带分组）  
         ggplot(exp_data_melted, aes(x=Var1, y=value, fill=group)) +   
-            geom_boxplot(outlier.shape = NA) +  
-            geom_point(position = position_dodge(width = 0.75), size = 1, color = "black") +  
-            labs(title="", x="Feature", y=assayname) +   
-            theme_minimal() +  
-            scale_fill_discrete(name = group_col)  # 如果提供了分组列则显示图例  
+              geom_boxplot(outlier.shape = NA) +  
+              geom_point(position = position_dodge(width = 0.75), size = 1, color = "black") +  
+              labs(title="", x="Feature", y=assayname) +   
+              theme(
+                axis.text.x = element_blank(),
+                axis.title.x = element_blank(),
+                panel.background = element_blank(),  
+                panel.grid.major = element_blank(), 
+                panel.grid.minor = element_blank(),   
+                panel.border = element_rect(color = "gray", fill = NA, size = 1)
+          ) +
+                scale_fill_discrete(name = group_col) +  
+                facet_wrap(~ Var1,scales  = "free_x",nrow = 1)  
     } else {  
         # 不进行分组，直接绘制  
-        exp_data_melted <- melt(as.matrix(exp_data_subset))  
-        
+        exp_data_melted <- melt(as.matrix(exp_data_subset))       
         # 绘制 boxplot（不带分组）  
         ggplot(exp_data_melted, aes(x=Var1, y=value)) +   
             geom_boxplot(outlier.shape = NA) +  # 省略离群点的显示  
             geom_point(position = position_dodge(width = 0.75), size = 1, color = "black") +
             labs(title="", x="Feature", y=assayname) +   
-            theme_minimal()  
+            theme(
+                axis.text.x = element_blank(),
+                axis.title.x = element_blank(),
+                panel.background = element_blank(),  
+                panel.grid.major = element_blank(), 
+                panel.grid.minor = element_blank(),   
+                panel.border = element_rect(color = "gray", fill = NA, size = 1)
+          ) +
+                scale_fill_discrete(name = group_col) +  
+                facet_wrap(~ Var1,scales  = "free_x",nrow = 1)  
     }  
 }  
