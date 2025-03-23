c58a301af398cda38da8d90c428eb40e2a5ac2c4
shaoxunyuan <ysx0226@gmail.com>
2025-03-23 20:16:20 +0800

    update all

--- R/SE_boxplot.R
+++ R/SE_boxplot.R
@@ -1,73 +1,141 @@
-#' Generates a boxplot for specified features in a SummarizedExperiment object.
-#'
-#' @param SE A SummarizedExperiment object.
-#' @param feature_of_interest Character vector of gene identifiers.
-#' @param assayname The assay name in the SummarizedExperiment object.
-#' @param group_col Column name in colData(SE) for grouping.
-#' @param normalization Normalization method ("scale", "log", or "none").
-#' @export
-SE_boxplot <- function(SE, feature_of_interest = c("AAGAB", "ABCA13", "ABCC4", "ABHD2"), assayname = "TPM", group_col = NULL, normalization = "none") {  
+#' SE_boxplot: Create Boxplots for Gene Expression Data  
+#'  
+#' This function generates violin and box plots for specified genes from a   
+#' SummarizedExperiment object, allowing for visualization of expression levels   
+#' across different groups. It performs normalization if specified, and uses   
+#' ANOVA with Tukey's post-hoc test to add significance markers on the plot.  
+#'  
+#' @param SE A SummarizedExperiment object containing gene expression data.  
+#' @param feature_of_interest A character vector of gene names to be plotted. Default is   
+#'            c("AAGAB", "ABCA13", "ABCC4", "ABHD2").  
+#' @param assayname A string specifying the assay from which to extract data.   
+#'            Default is "TPM".  
+#' @param group_colname A string specifying the column name in colData for group   
+#'            information. Default is NULL.  
+#' @param normalization A string specifying the normalization method. Options include   
+#'            "none", "scale", or "log". Default is "none".  
+#'  
+#' @import SummarizedExperiment  
+#' @import dplyr  
+#' @import tidyr  
+#' @import ggplot2  
+#' @import multcompView  
+#' @import plyr  
+#' @importFrom stats aov TukeyHSD  
+#' @importFrom stats log  
+#'  
+#' @return A ggplot object representing the violin and box plots with significance markers.  
+#'  
+#' @examples  
+#' # Load necessary libraries  
+#' library(SummarizedExperiment)  
+#' library(ggplot2)  
+#' library(dplyr)  
+#'   
+#' # Create a dummy SummarizedExperiment object  
+#' data_matrix <- matrix(rnorm(1000), nrow = 100, ncol = 10)  
+#' rownames(data_matrix) <- paste0("Gene", 1:100)  
+#' colnames(data_matrix) <- paste0("Sample", 1:10)  
+#' sample_info <- DataFrame(group = rep(c("A", "B"), each = 5))  
+#' SE <- SummarizedExperiment(assays = list(TPM = data_matrix), colData = sample_info)  
+#'  
+#' # Call the SE_boxplot function  
+#' plot <- SE_boxplot(SE, feature_of_interest = c("Gene1", "Gene2"), group_colname = "group", normalization = "log")  
+#' print(plot)  
+#'  
+#' @export  
+SE_boxplot <- function(SE,   
+                       feature_of_interest = c("AAGAB", "ABCA13", "ABCC4", "ABHD2"),   
+                       assayname = "TPM",   
+                       group_colname = NULL,   
+                       normalization = "none") {  
   
     if (!inherits(SE, "SummarizedExperiment")) {  
         stop("Input SE must be a SummarizedExperiment object.")  
     }  
     
-    # 提取表达数据  
     exp_data_subset <- assay(SE, assayname)[feature_of_interest, , drop = FALSE]  
-    
-    # 检查基因是否存在于数据中  
+     
     missing_genes <- setdiff(feature_of_interest, rownames(exp_data_subset))  
     if (length(missing_genes) > 0) {  
-        warning(paste("The following genes are not found in the dataset:", paste(missing_genes, collapse = ", ")))  
+        warning(paste("The following genes are not found in the dataset:",   
+                      paste(missing_genes, collapse = ", ")))  
         feature_of_interest <- intersect(feature_of_interest, rownames(exp_data_subset))  
         exp_data_subset <- exp_data_subset[feature_of_interest, , drop = FALSE]  
     }  
-
-    # 标准化表达数据  
+   
     if (normalization == "scale") {  
-        exp_data_subset <- t(apply(exp_data_subset, 1, scale))  # 对每个基因进行 z-score 标准化  
+        exp_data_subset <- t(apply(exp_data_subset, 1, scale))   
     } else if (normalization == "log") {  
-        exp_data_subset <- log2(exp_data_subset)  # 对数据进行 log1p 转换  
-    }  # 如果 sel_mode 是 "none"，则保留原始数据  
-
-    # 如果提供了分组且不是 NULL，则获取分组信息  
-    if (!is.null(group_col) && group_col %in% names(colData(SE))) {  
-        group_info <- colData(SE)[[group_col]]  
-        exp_data_melted <- reshape2::melt(as.matrix(exp_data_subset))  
-        exp_data_melted$group <- rep(group_info, each = nrow(exp_data_subset))  
-        
-        # 绘制 boxplot（带分组）  
-        ggplot(exp_data_melted, aes(x=Var1, y=value, fill=group)) +   
-              geom_boxplot(outlier.shape = NA) +  
-              geom_point(position = position_dodge(width = 0.75), size = 1, color = "black") +  
-              labs(title="", x="Feature", y=assayname) +   
-              theme(
-                axis.text.x = element_blank(),
-                axis.title.x = element_blank(),
-                panel.background = element_blank(),  
-                panel.grid.major = element_blank(), 
-                panel.grid.minor = element_blank(),   
-                panel.border = element_rect(color = "gray", fill = NA, size = 1)
-          ) +
-                scale_fill_discrete(name = group_col) +  
-                facet_wrap(~ Var1,scales  = "free_x",nrow = 1)  
-    } else {  
-        # 不进行分组，直接绘制  
-        exp_data_melted <- melt(as.matrix(exp_data_subset))       
-        # 绘制 boxplot（不带分组）  
-        ggplot(exp_data_melted, aes(x=Var1, y=value)) +   
-            geom_boxplot(outlier.shape = NA) +  # 省略离群点的显示  
-            geom_point(position = position_dodge(width = 0.75), size = 1, color = "black") +
-            labs(title="", x="Feature", y=assayname) +   
-            theme(
-                axis.text.x = element_blank(),
-                axis.title.x = element_blank(),
-                panel.background = element_blank(),  
-                panel.grid.major = element_blank(), 
-                panel.grid.minor = element_blank(),   
-                panel.border = element_rect(color = "gray", fill = NA, size = 1)
-          ) +
-                scale_fill_discrete(name = group_col) +  
-                facet_wrap(~ Var1,scales  = "free_x",nrow = 1)  
+        exp_data_subset <- log2(exp_data_subset + 1)   
     }  
-}  
+
+    sample_info <- colData(SE)  
+   
+    exp_data_long <- as.data.frame(exp_data_subset) %>%   
+                     rownames_to_column(var = "feature") %>%   
+                     pivot_longer(cols = -feature, names_to = "sample", values_to = "express")  
+
+    exp_data_long$group <- plyr::mapvalues(exp_data_long$sample,   
+                                            from = rownames(sample_info),   
+                                            to = sample_info[[group_colname]],   
+                                            warn_missing = FALSE)   
+	  
+    MakeSigMarker <- function(onedata) {  
+        if (n_distinct(onedata$group) < 2) {  
+            warning("Only one group found. Skipping analysis.")  
+            return(data.frame(group = unique(onedata$group), label = NA,   
+                              y_position = NA, feature = unique(onedata$feature)))  
+        }  
+        
+        formula_str <- as.formula(paste("express ~ group"))  
+        anova_result <- aov(formula_str, data = onedata)  
+         
+		# Get pvalue   
+        tukey_result <- TukeyHSD(anova_result)  
+        p_values <- tukey_result[[group_colname]][, "p adj"]  
+        names(p_values) <- rownames(tukey_result[[group_colname]])  
+         
+        letters <- multcompLetters(p_values)$Letters  
+        letter_df <- data.frame(group = names(letters), label = letters)  
+
+        max_vals <- onedata %>%  
+                    group_by(group) %>%  
+                    summarise(y_position = max(express, na.rm = TRUE) + 0.1)   
+        
+        results <- left_join(max_vals, letter_df, by = "group")  
+        results$feature <- unique(onedata$feature)  
+        
+        return(results)  
+    }  
+    
+    dfout_summary <- data.frame()  
+    
+    for (feature in unique(exp_data_long$feature)) {  
+        onedata <- exp_data_long[exp_data_long$feature == feature,]  
+        out <- MakeSigMarker(onedata)  
+        dfout_summary <- rbind(dfout_summary, out)  
+    }  
+    dfout_summary <- dfout_summary[!is.na(dfout_summary$label),]  
+   
+    plot <- ggplot(exp_data_long, aes(x = group, y = express, fill = feature)) +   
+            geom_violin(alpha = 0.5, trim = FALSE, position = position_dodge(width = 0.8), fill = NA, color  = "lightgray") +  
+            geom_boxplot(width = 0.1, position = position_dodge(width = 0.8), outlier.shape = NA, color = "darkgray", alpha = 0.5) +   
+            geom_text(data = dfout_summary, aes(x = group, y = y_position + 0.1 , label = label), size = 5, position = position_nudge(y = 0), inherit.aes = FALSE) +  
+            facet_row(~ feature, scales = "free", space = "free") +  
+            theme_minimal() +  
+            theme(  
+                legend.position = "none",  
+                legend.title = element_text(size = 12, colour = "black"),  
+                strip.text = element_text(size = 12, colour = "black"),  
+                panel.border = element_rect(color = "gray", fill = NA, size = 1),  
+                axis.text.x = element_text(size = 12, colour = "black"),  
+                axis.text.y = element_text(size = 12, colour = "black"),  
+                axis.title.x = element_blank(),  
+                axis.title.y = element_text(size = 12, colour = "black"),  
+                panel.grid.major = element_blank(),  
+                panel.grid.minor = element_blank()  
+            )  
+
+    return(plot)  
+}
