93d6040ab6cae0a83ee18e4939357ff11540ceb2
shaoxunyuan <ysx0226@gmail.com>
2025-03-24 10:16:07 +0800

    update

--- R/loadSE.R
+++ R/loadSE.R
@@ -14,7 +14,29 @@
 loadSE <- function() {  
   library(SummarizedExperiment)
   # 设文件路径  
-  file_path <- system.file("extdata", "example.SE", package = "SEtoolbox")  
+  file_path <- system.file("extdata", "SE.rds", package = "SEtoolbox")  
+  
+  # 检查文件是否存在  
+  if (file.exists(file_path)) {  
+    # 读取 RDS 文件  
+    example_SE <- readRDS(file_path)  
+    
+    # 检查是否为 SummarizedExperiment 对象  
+    if (!is(example_SE, "SummarizedExperiment")) {  
+      stop("The loaded file is not a SummarizedExperiment object.")  
+    }  
+    
+    # 返回 SummarizedExperiment 对象  
+    return(example_SE)  
+  } else {  
+    stop("Example file not found.")  
+  }  
+}
+
+loadSElist <- function() {  
+  library(SummarizedExperiment)
+  # 设文件路径  
+  file_path <- system.file("extdata", "SElist.rds", package = "SEtoolbox")  
   
   # 检查文件是否存在  
   if (file.exists(file_path)) {  
