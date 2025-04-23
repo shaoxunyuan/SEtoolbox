2fee9b6272db969dfe8641846b50798e0c8e50dd
shaoxunyuan <ysx0226@gmail.com>
2025-04-23 09:10:13 +0800

    update

--- R/SE_DESeq2.R
+++ R/SE_DESeq2.R
@@ -12,20 +12,19 @@
 #' @param SE A SummarizedExperiment object containing count data.  
 #' @param assayname The name of the assay to use for the analysis. Default is "Count".  
 #' @param group_colname The name of the column in colData(SE) that contains the factor for grouping samples. Default is "group".  
+#' @param set_compare A character vector specifying the groups to compare. It should contain valid group names present in colData.  
 #' @return A SummarizedExperiment object with DESeq2 results stored in its metadata.  
 #' @examples  
 #' # Load necessary libraries  
-#' library(SummarizedExperiment)  
-#' library(DESeq2)  
 #'  
 #' # Load example SummarizedExperiment object  
-#' SE = loadSE() 
+#' SE = loadSE()   
 #'  
 #' # Perform differential expression analysis  
-#' results_se <- SE_DEseq2(SE = SE, assayname = "Count", group_colname = "group")  
+#' results_se <- SE_DEseq2(SE = SE, assayname = "Count", group_colname = "group", set_compare = c("hc", "patient"))  
 #'  
 #' @export  
-SE_DEseq2 <- function(SE, assayname = "Count", group_colname = "group") {  
+SE_DEseq2 <- function(SE, assayname = "Count", group_colname = "group", set_compare = c("hc", "patient")) {  
 
   # Validate that SE is of type SummarizedExperiment  
   if (!is(SE, "SummarizedExperiment")) {  
@@ -41,9 +40,19 @@ SE_DEseq2 <- function(SE, assayname = "Count", group_colname = "group") {
   if (!group_colname %in% colnames(colData(SE))) {  
     stop("The grouping column does not exist in colData: ", group_colname)  
   }  
+  
+  # Check that all groups in set_compare are present in colData  
+  if (!all(set_compare %in% unique(colData(SE)[[group_colname]]))) {  
+    stop("One or more groups in 'set_compare' are not present in the specified grouping column ", group_colname, ".")  
+  }  
 
   # Extract count matrix and force to integer (DESeq2 requires integers)  
-  countData <- assay(SE, assayname)  
+  countData <- assay(SE, assayname)
+  if (any(countData < 0)) {  
+    min_value <- abs(min(countData[countData < 0])) + 1  # 获取负值的绝对值加1  
+    message("Negative values detected, applying transformation... Adding minimum value: ", min_value)  
+    countData <- countData + min_value  # 添加最小值以消除负值  
+  }  
   if (!all(countData == floor(countData))) {  
     message("Non-integer values detected, applying ceiling transformation...")  
     countData <- ceiling(countData) + 1  
@@ -66,26 +75,29 @@ SE_DEseq2 <- function(SE, assayname = "Count", group_colname = "group") {
   # Results processing  
   # --------------------------  
   # Get grouping information and validate  
-  groups <- unique(sample_info[[group_colname]])  
-  
-  num_groups <- length(groups)  
+  num_groups <- length(set_compare)  
   
   # Store all differential analysis results in a list  
   all_results <- list()  
   # Perform all possible pairwise comparisons  
-  for (i in 1:(num_groups - 1)) {  
-    for (j in (i + 1):num_groups) {  
-      contrast <- c(group_colname, groups[i], groups[j])  
-      res <- results(dds, contrast = contrast)  
-      # Identify rows with NA in log2FoldChange  
-      na_rows <- which(is.na(res$pvalue))  
-      # Modify specified column values for these rows  
-      res[na_rows, c("log2FoldChange", "lfcSE", "stat", "pvalue", "padj")] <- 0  
-      res[na_rows, "pvalue"] <- 1  
-      res[na_rows, "padj"] <- 1  
-      res <- res[order(res$pvalue, decreasing = FALSE),]  
-      comparison_name <- paste0(groups[i], "_vs_", groups[j])  
-      all_results[[comparison_name]] <- res  
+  for (i in num_groups:2) {  
+    for (j in (i - 1):1) {  
+      contrast <- c(group_colname, set_compare[j], set_compare[i])  
+       
+      if (contrast[2] %in% levels(dds[[group_colname]]) && contrast[3] %in% levels(dds[[group_colname]])) {  
+        res <- as.data.frame(results(dds, contrast = contrast))
+		res$gene = rownames(res);rownames(res) = NULL
+        na_rows <- which(is.na(res$pvalue))  
+        res[na_rows, c("log2FoldChange", "lfcSE", "stat", "pvalue", "padj")] <- 0  
+        res[na_rows, "pvalue"] <- 1  
+        res[na_rows, "padj"] <- 1  
+
+        res <- res[order(res$pvalue, decreasing = FALSE),]  
+        comparison_name <- paste0(set_compare[j], "_vs_", set_compare[i])  
+        all_results[[comparison_name]] <- res  
+      } else {  
+        warning(paste("Compare group", contrast[3], " vs ", contrast[2], " not exist"))  
+      }  
     }  
   }  
 
